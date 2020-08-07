ad86d8ae34e428a8d24ec270e1859c88ad934ab0
Adam Botley <adambotley@gmail.com>
2020-08-07 12:31:36 +0100

    task 2 setup

--- app/services/todo-list.service.spec.ts
+++ app/services/todo-list.service.spec.ts
@@ -3,6 +3,7 @@ import {todoListServiceModuleName} from "."
 import { MockStorage } from "app/test-utils"
 
 describe('TodoListService', () => {
+  let $httpBackend: ng.IHttpBackendService
   let $q: ng.IQService
   let $timeout: ng.ITimeoutService
   let $window: Partial<ng.IWindowService>
@@ -29,12 +30,36 @@ describe('TodoListService', () => {
 
   // Get local references to angular components/services
   beforeEach(inject((
+    _$httpBackend_: ng.IHttpBackendService,
     _$q_: ng.IQService,
     _$timeout_: ng.ITimeoutService,
     _todoListService_: TodoListService,
   ) => {
+    $httpBackend = _$httpBackend_
     $q = _$q_
     $timeout = _$timeout_
     todoListService = _todoListService_
   }))
+
+  it('sends post request to store current todo items', () => {
+    // setup current data
+    const storedTodos = [{
+      id: 0,
+      title: 'foo',
+      done: false,
+    }]
+    localStorageMock.setItem(TodoListService.TASKS_KEY, JSON.stringify(storedTodos))
+
+    // create new todo to store
+    const newTodo = {
+      id: 1,
+      title: 'bar',
+      done: false,
+    }
+
+    todoListService.storeRemote(newTodo)
+
+    // flush http queue
+    $httpBackend.flush()
+  })
 })
