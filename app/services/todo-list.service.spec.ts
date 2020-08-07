5eba1a16f64e89fb5f620f16d05e1fb635b7c8d4
Adam Botley <adambotley@gmail.com>
2020-08-07 10:23:39 +0100

    task 1 answer

--- app/services/todo-list.service.spec.ts
+++ app/services/todo-list.service.spec.ts
@@ -1,18 +1,31 @@
 import TodoListService from "./todo-list.service"
 import {todoListServiceModuleName} from "."
+import { MockStorage } from "app/test-utils"
 
 describe('TodoListService', () => {
   let $q: ng.IQService
   let $timeout: ng.ITimeoutService
+  let $window: Partial<ng.IWindowService>
   let todoListService: TodoListService
+  let localStorageMock: Storage
 
   // Mock the module being tested. Remember this loads the module dependencies for us
   beforeEach(angular.mock.module(todoListServiceModuleName))
 
   // mock dependencies by using $provide here
   beforeEach(angular.mock.module(($provide: ng.auto.IProvideService) => {
-    //...
+    // Instantiate a new MockStorage object before each test
+    localStorageMock = new MockStorage()
+
+    // assign it to a mock $window object
+    $window = {
+      localStorage: localStorageMock,
+    }
+
+    // replace the normal $window with out own using $provide
+    $provide.constant('$window', $window)
   }))
+
 
   // Get local references to angular components/services
   beforeEach(inject((
